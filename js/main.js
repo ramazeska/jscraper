dba18ce94620885a4e4d0ad4cfe7e0b77f8db3a9
anton <antonchern@gmail.com>
2021-07-06 18:10:27 +0300

    curent

--- js/main.js
+++ js/main.js
@@ -1,11 +1,54 @@
-function get_resource(){
-    fetch("http://localhost:3000/q/search.aspx?keyword=ball")
+const proxyUrl = "http://localhost:3000/q";
+const modsPattern = '/\{\"mods\":.*\}/gm';
+async function Main(){
+    document.addEventListener("submit", async () => {
+        let input = document.querySelector("input").value
+        let result = await get_resource(input)
+        log("main")
+        log(JSON.parse(result[0]));
+        //document.body.innerHTML = await get_resource(input).body.innerHTML;
+
+    })
+}
+
+function log(msg){console.log(msg)}
+
+function buildUrl(param){
+    return proxyUrl + "/wholesale?SearchText="+param;
+    //return proxyUrl + "/search.aspx?keyword="+param;
+}
+
+async function get_resource(val){
+    url = buildUrl(val)
+    return fetch(url)
     .then(
         function (response){
-            console.log("response:"+ response.body)
+            log("response:"+ response.statusCode)
+            return response.text()
         })
+    .then(function (data){
+        const parser = new DOMParser();
+        return parser.parseFromString(data, "text/html")
+        
+    }).then(function (html){
+        
+        let windowsParams;
+        let scripts = html.querySelectorAll("script");
+        for (let item of scripts){
+            if (item.innerText.toLowerCase().includes("window.runparams")) {
+                windowsParams = item.innerText;
+                let regex = new RegExp('\{"mods".*\}')
+                windowsParams = item.innerText.match(regex)
+                break;
+            }
+        }
+
+
+           
+        return windowsParams;
+    })
     .catch(err => {
             console.error(err)
         })
-
+    
 }
--- js/proxy.js
+++ js/proxy.js
@@ -2,21 +2,30 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const options = {
-    target: 'https://zap.co.il', // target host
+    target: 'https://aliexpress.com', // target host
     changeOrigin: true, // needed for virtual hosted sites
 
     pathRewrite: {
         '^/q/': '', // rewrite path
     },
-    onProxyRes
+    followRedirects: true,
+    onProxyRes,
+    onProxyReq
 };
 
+function onProxyReq(proxyReq, req, res){
+    console.log("Request")
+    console.log(proxyReq.headers)
+}
+
 function onProxyRes(proxyRes, req, res) {
-    console.log(proxyRes.headers)
+    console.log("not modified\n"+JSON.stringify(proxyRes.headers))
     // proxyRes.headers['x-added'] = 'foobar'; // add new header to response
     // console.log(proxyRes.headers)
     // delete proxyRes.headers['x-removed']; // remove header from response
+    delete proxyRes.headers['location']
     proxyRes.headers['Access-Control-Allow-Origin'] = '*'
+    proxyRes.headers['access-control-allow-origin'] = '*'
     console.log(proxyRes.headers)
 }
 
