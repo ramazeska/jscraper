386b1b6dbc060f14810912a0cb159cbe22d7bb78
achernomaz <achernomaz@ebay.com>
2021-07-07 18:42:14 +0300

    get doc, filter and display a flexbox

--- js/main.js
+++ js/main.js
@@ -1,21 +1,67 @@
 const proxyUrl = "http://localhost:3000/q";
-const modsPattern = '/\{\"mods\":.*\}/gm';
+const regex = new RegExp('\{"mods".*\}')
 async function Main(){
     document.addEventListener("submit", async () => {
         let input = document.querySelector("input").value
         let result = await get_resource(input)
         log("main")
-        log(JSON.parse(result[0]));
+        // log(result['mods']);
+        const filtered = result.mods.itemList.content.map(function (mods){
+
+            return {
+                "image":"https:"+mods.image.imgUrl, "price": mods.prices.salePrice.minPrice,
+                "currency": mods.prices.salePrice.currencyCode,
+                "storeName": mods.store.storeName, "storeUrl": "https:"+mods.store.storeUrl,
+                "title": mods.title.displayTitle,
+                "sold": mods.trade.tradeDesc
+            }
+            // return obj
+        })
+        log(filtered)
+        await buildView(filtered)
         //document.body.innerHTML = await get_resource(input).body.innerHTML;
 
     })
+}
+
+function genName(){
+    const chars = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"]
+    return [...Array(10)].map(i=>chars[Math.random()*chars.length|0]).join``;
+}
+
+function genContainer(item){
+    let container = document.createElement("div")
+    container.classList.add("container")
+    let tpl = `<div class=${genName()}>\n` +
+        `        <div><img src=${item.image} alt=${item.title}></div>\n` +
+        `        <div><a href=${item.storeUrl}>${item.storeName}</a></div>\n` +
+        `        <div><p>${item.title}</div>\n` +
+        `        <div><p>${item.currency} ${item.price}</div>\n` +
+        `        <div><p>sold: ${item.sold}</div>\n` +
+        "      </div>"
+
+    container.innerHTML=tpl
+    return container
+}
+
+async function buildView(srcJson){
+    let parentElem = document.getElementsByClassName("topLevelDiv").item(0)
+    myDiv = document.createElement("div")
+    myDiv.classList.add("resultContainer")
+    for (let item of srcJson){
+        let subDiv = document.createElement("div")
+        subDiv.classList.add("childItem")
+        subDiv.appendChild(genContainer(item))
+        myDiv.appendChild(subDiv)
+    }
+    parentElem.appendChild(myDiv)
+
 }
 
 function log(msg){console.log(msg)}
 
 function buildUrl(param){
     return proxyUrl + "/wholesale?SearchText="+param;
-    //return proxyUrl + "/search.aspx?keyword="+param;
 }
 
 async function get_resource(val){
@@ -29,26 +75,25 @@ async function get_resource(val){
     .then(function (data){
         const parser = new DOMParser();
         return parser.parseFromString(data, "text/html")
-        
+
     }).then(function (html){
-        
+
         let windowsParams;
         let scripts = html.querySelectorAll("script");
         for (let item of scripts){
             if (item.innerText.toLowerCase().includes("window.runparams")) {
                 windowsParams = item.innerText;
-                let regex = new RegExp('\{"mods".*\}')
+
                 windowsParams = item.innerText.match(regex)
                 break;
             }
         }
 
 
-           
-        return windowsParams;
+        return JSON.parse(windowsParams);
     })
     .catch(err => {
             console.error(err)
         })
-    
+
 }
